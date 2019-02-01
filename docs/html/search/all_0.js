e57d11caefe55b122c06e1408a3f509db87af8ca
sarahkittyy <chrisohlin1@gmail.com>
2019-02-01 10:22:16 -0500

    added docs

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['application',['Application',['../class_application.html',1,'Application'],['../class_application.html#afa8cc05ce6b6092be5ecdfdae44e05f8',1,'Application::Application()']]]
+];
---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['run',['run',['../class_application.html#a8cf8941c8db90117d3735bce5ae1fdf4',1,'Application']]]
+];
