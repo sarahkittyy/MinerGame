97585f8e0c297a4f4069b2b8bf73956f80bdf18c
sarahkittyy <chrisohlin1@gmail.com>
2019-02-05 20:07:32 -0500

    cmake building

---
+++ build/linux/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sarahkitty/Code/C++/Miner/include/imgui-sfml/imgui-SFML.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/include/imgui-sfml/imgui-SFML.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/include/imgui/imgui.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/include/imgui/imgui.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/include/imgui/imgui_demo.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/include/imgui/imgui_demo.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/include/imgui/imgui_draw.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/include/imgui/imgui_draw.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/include/imgui/imgui_widgets.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/include/imgui/imgui_widgets.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/Application.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/Application.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/BuildingManager.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/BuildingManager.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/KeyManager.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/KeyManager.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/MaterialManager.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/MaterialManager.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/Tilemap.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/Tilemap.cpp.o"
+  "/home/sarahkitty/Code/C++/Miner/src/main.cpp" "/home/sarahkitty/Code/C++/Miner/build/linux/CMakeFiles/a.out.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../include/imgui"
+  "../../include/imgui-sfml"
+  "../../include/nlohmann"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/windows/CMakeFiles/3.13.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-4.14.90-1-lts")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "4.14.90-1-lts")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/sarahkitty/Code/C++/Miner/winbuild/win-toolchain.cmake")
+
+set(CMAKE_SYSTEM "Windows")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
