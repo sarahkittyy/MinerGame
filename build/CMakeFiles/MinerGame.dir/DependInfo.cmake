650b0f706b05366665ed854bc7d8d2fc26cc8b2f
sarahkittyy <chrisohlin1@gmail.com>
2019-02-05 19:06:15 -0500

    cmake base

---
+++ build/CMakeFiles/MinerGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sarahkitty/Code/C++/Miner/include/imgui-sfml/imgui-SFML.cpp" "/home/sarahkitty/Code/C++/Miner/build/CMakeFiles/MinerGame.dir/include/imgui-sfml/imgui-SFML.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/imgui"
+  "../include/imgui-sfml"
+  "../include/nlohmann"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
